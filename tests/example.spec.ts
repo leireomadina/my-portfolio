f47204b6370fb364bcb379a4ce543b1d42c14cdc
leireomadina <leireomadina@gmail.com>
2024-04-22 17:40:13 +0200

    🧪 WIP: add sample tests (need fix)

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,44 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('page loads', async ({ page }) => {
+	await expect(page).toBeDefined();
+});
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test('has title', async ({ page }) => {
+	await expect(page).toHaveTitle(/Leire Ordeñana Madina | Full-stack developer/);
+});
 
+test('get started link', async ({ page, browser }) => {
+	const context = await browser.newContext();
+
+	context.on('page', async page => {
+		await page.waitForLoadState();
+		console.log('uwu')
+		process.stdout.write('uwuuuu')
+		console.log(await page.title());
+	});
+
+
+	/*const pagePromise = context.waitForEvent('page');
+  await page.getByRole('link', { name: 'Zorraquino' }).click();
+	const newPage = await pagePromise;
+	console.log('newPage ', newPage)
+	await newPage.waitForLoadState();
+	console.log(await newPage.title());*/
+
+
+
+	// TODO: https://playwright.dev/docs/pages#handling-new-pages
+
+	// await expect(page).toHaveURL(/zorraquino.com/);
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // await expect(page.getByRole('heading', { name: 'Zorraquino' })).toBeVisible();
+  // await expect(page.getByText('Zorraquino')).toBeVisible();
+	// await page.waitForSelector('header :text("Zorraquino")');
+
 });
