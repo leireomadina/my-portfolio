446b54d17d0d9682edae567b712825e6e5c5c1f6
leireomadina <leireomadina@gmail.com>
2025-02-16 15:09:18 +0100

    Update vercel image service config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
@@ -13,9 +14,12 @@ export default defineConfig({
 						@use "./src/styles/base/_functions.scss" as *;
 						@use "./src/styles/base/_mixins.scss" as *;
 						@use "./src/styles/base/_variables.scss" as *;
-					`
-				}
-			}
-		}
-	}
+					`,
+				},
+			},
+		},
+	},
+	adapter: vercel({
+		imageService: true,
+	}),
 })
