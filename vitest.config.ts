d84b34340e6d6f061757230ad97e8367d0745155
Leire Ordeñana Madina <leireomadina@gmail.com>
2025-06-22 12:50:47 +0200

    Install and configure happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { getViteConfig } from 'astro/config'
 
 export default getViteConfig({
 	test: {
+		environment: 'happy-dom',
 		/* for example, use global to avoid globals imports (describe, test, expect): */
 		// globals: true,
 	},
