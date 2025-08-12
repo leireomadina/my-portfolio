dcb148ac794f445aa33bc01129b934ef5c75b5f2
Leire Ordeñana Madina <leireomadina@gmail.com>
2025-08-12 17:11:42 +0200

    Update test helpers

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,24 +1,24 @@
-  // Source: https://angelika.me/2025/02/01/astro-component-unit-tests/
-import type { ComponentProps } from "astro/types"
-import {
-    experimental_AstroContainer as AstroContainer,
-    type ContainerRenderOptions
-} from "astro/container"
+// Source: https://angelika.me/2025/02/01/astro-component-unit-tests/
+import type { ComponentProps } from 'astro/types'
+import { experimental_AstroContainer as AstroContainer, type ContainerRenderOptions } from 'astro/container'
 
-type AstroComponentFactory = Parameters<AstroContainer["renderToString"]>[0]
+type AstroContainerInstance = Awaited<ReturnType<typeof AstroContainer.create>>
 
-type ComponentContainerRenderOptions<T extends AstroComponentFactory> = Omit<ContainerRenderOptions, 'props'> & {
-  // @ts-expect-error ComponentProps expects a type that extends a function of arity 1, but
-  // AstroComponentFactory is a function of arity 3. Either this is an internal mix up in Astro,
-  // or I'm missing something.
+type AstroComponentFactory = Parameters<AstroContainerInstance['renderToString']>[0]
+
+type RenderOptions<T extends AstroComponentFactory> = Omit<ContainerRenderOptions, 'props'> & {
+  // @ts-expect-error ComponentProps no encaja exactamente
   props?: ComponentProps<T>
 };
 
-export async function renderAstroComponent<T extends AstroComponentFactory>(Component: T, options: ComponentContainerRenderOptions<T> = {}) {
+export async function renderAstroComponent<T extends AstroComponentFactory>(
+  Component: T,
+  options: RenderOptions<T> = {},
+) {
   const container = await AstroContainer.create()
   const result = await container.renderToString(Component, options)
 
-  const template = document.createElement("template")
+  const template = document.createElement('template')
   template.innerHTML = result
 
   return template.content
