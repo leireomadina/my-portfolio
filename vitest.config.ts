6a6ae894a97e6d9832302506a62f6590c2f1317f
Leire Ordeñana Madina <leireomadina@gmail.com>
2026-08-16 12:54:44 +0200

    Fix vitest environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { getViteConfig } from 'astro/config'
 
 export default getViteConfig({
 	test: {
-		environment: 'happy-dom',
+		environment: './test/happy-dom-ssr-environment.ts',
 		/* for example, use global to avoid globals imports (describe, test, expect): */
 		// globals: true,
 	},
